e07493f223a605fd2e55473a9233a424e458e09f
Norstroph <luc.alexandre.dulon@gmail.com>
2025-07-08 12:20:31 +0200

    Initial commit from create-react-router

--- src/pages/B_body/Mouvies.tsx
+++ src/pages/B_body/Mouvies.tsx
@@ -4,12 +4,18 @@ import {get} from "../../api/api.tsx";
 import MovieItem from "../../components/MovieItem.tsx";
 import Pages from "../../components/layout/Pages.tsx";
 import {useParams} from 'react-router';
+import genreData from "../../dataFake/genre.json";
+import {Box, Chip} from "@mui/material";
 
 const Movies = () => {
     const [allMovise, setMovies] = useState<Movie[]>([]);
     const [searchMouvies, setSearchMouvies] = useState<Movie[]>([]);
+    const [genreMovies, setGenreMovies] = useState<Movie[]>([]);
+    const [selectedGenre, setSelectedGenre] = useState<number | null>(null);
+    const [genreName, setGenreName] = useState<string | null>(null);
     const [isPending, startTransition] = useTransition()
     const {search} = useParams();
+
     console.log(search)
 
     useEffect(() => {
@@ -25,25 +31,43 @@ const Movies = () => {
 
 
     }, [search]);
+    const handleGenreClick = async (genreId: number, genreName: string) => {
+        setSelectedGenre(genreId);
+        setGenreName(genreName);
+        const res = await get(`/discover/movie?with_genres=${genreId}&page=1`);
+        setGenreMovies(res.results);
+    };
     console.log(search)
     console.log(searchMouvies)
     return (
 
-        <Pages title={"Movies"}>
-            <div>
-                {!isPending ? (
-                    <>
-                        <h1>Films</h1>
-                        <div style={{display: 'flex', flexWrap: 'wrap', gap: '20px'}}>
-                            {!search || "" ? allMovise.map((movie) => (
+        <Pages title={"Movies"}>                                                                                  
+
+
+            {!isPending ? (
+                <>
+                    <Box sx={{position: 'Sticky', top: 0, zIndex: 1000, gap: '2rem'}}>
+                        {genreData.genres.map((genre) => (
+                            <Chip key={genre.id} label={genre.name} color="primary" size="small"
+                                  onClick={() => handleGenreClick(genre.id, genre.name)}/>))}
+                    </Box>
+                    <h1>{!genreName ? "Films" : genreName}</h1>
+                    <Box sx={{display: 'flex', flexWrap: 'wrap', gap: '20px'}}>
+                        {selectedGenre
+                            ? genreMovies.map((movie) => (
                                 <MovieItem key={movie.id} movie={movie}/>
-                            )) : searchMouvies.map((movie) => (
-                                <MovieItem key={movie.id} movie={movie}/>
-                            ))}
-                        </div>
-                    </>
-                ) : (<p style={{color: "white", padding: "2rem"}}>Chargement...</p>)}
-            </div>
+                            ))
+                            : !search
+                                ? allMovise.map((movie) => (
+                                    <MovieItem key={movie.id} movie={movie}/>
+                                ))
+                                : searchMouvies.map((movie) => (
+                                    <MovieItem key={movie.id} movie={movie}/>
+                                ))}
+                    </Box>
+                </>
+            ) : (<p style={{color: "white", padding: "2rem"}}>Chargement...</p>)}
+
         </Pages>
 
     )
